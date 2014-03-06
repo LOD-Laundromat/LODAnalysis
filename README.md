7f454030fc10efe6069231dcc7471132f1fd8e00
Laurens Rietveld <laurens.rietveld@gmail.com>
2014-03-06 17:13:54 +0100

    implemented counting of namespaces

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 LODAnalysis
 ===========
+
+##Requirements
+* PIG[1]
+* Hadoop[2]
+* Python[3]
+* D2S4PIG[4]: clone project, run `mvn clean package` to compile, and add it to the PIG classpath
+
+###NameSpace extraction
+To run, execute
+`pig LODAnalysis/pig/extractNs.py <hadoop_input_file>`. Output is stored in path `<hadoop_input_file>_analysis/namespaces`.
+For now, this script only counts the namespaces occuring in predicate position. (we can easily change this)
+    
+    
+    
+##Links
+1. [http://pig.apache.org/](http://pig.apache.org/)
+2. [http://hadoop.apache.org/](http://hadoop.apache.org/)
+3. [http://www.python.org/](http://www.python.org/)
+4. [https://github.com/Data2Semantics/D2S4Pig](https://github.com/Data2Semantics/D2S4Pig)
--- pig/extractNs.py
+++ pig/extractNs.py
@@ -11,21 +11,26 @@ if len(sys.argv) <= 1:
 
 
 inputFile = sys.argv[1]
-
-outputFile = "%s/namespaces" % (dirname(inputFile))
+outputDir = "%s_analysis" % (inputFile)
+outputFile = "%s/namespaces" % (outputDir)
 if (len(sys.argv) == 3):
     outputFile = sys.argv[2]
     
 pigScript = """
 REGISTER d2s4pig/target/d2s4pig-1.0.jar
 DEFINE NtLoader com.data2semantics.pig.loaders.NtLoader();
-"""
-
-pigScript += """
 graph = LOAD '$inputFile' USING NtLoader() AS (sub:chararray, pred:chararray, obj:chararray);
 
+mkdir $outputDir
+
+
+propNameSpaces = FOREACH graph GENERATE REGEX_EXTRACT (pred, '<(.*)[#/].*>', 1) AS namespace;
+
+propNameSpaces = GROUP propNameSpaces BY namespace;
+
+nameSpaceCounts = FOREACH propNameSpaces GENERATE group, COUNT(propNameSpaces);
 rmf $outputFile
-STORE namespaces INTO '$outputFile' USING PigStorage();
+STORE nameSpaceCounts INTO '$outputFile' USING PigStorage();
 """
 
 
