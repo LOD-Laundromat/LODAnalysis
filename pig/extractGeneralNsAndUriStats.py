b3563107df60d31816fe569f204948ced660e295
Laurens Rietveld <laurens.rietveld@gmail.com>
2014-03-12 14:00:43 +0100

    calc some more stats related to uris and namespaces. added to single script for performance reasons

--- pig/extractGeneralNsAndUriStats.py
+++ pig/extractGeneralNsAndUriStats.py
@@ -12,7 +12,9 @@ if len(sys.argv) <= 1:
 
 inputDir = sys.argv[1]
 inputFile = "%s/input.nt" % (inputDir)
-outputFile = "%s/namespaces" % (inputDir)
+namespaceCountsOutput = "%s/namespaceCounts" % (inputDir)
+namespaceUniqCountsOutput = "%s/namespaceUniqCounts" % (inputDir)
+uniqUris = "%s/urisUniq" % (inputDir)
 if (len(sys.argv) == 3):
     outputFile = sys.argv[2]
     
@@ -26,17 +28,40 @@ resources = FOREACH graph GENERATE FLATTEN(TOBAG(*));
 ---filter out literals
 filteredResources = FILTER resources BY SUBSTRING($0, 0, 1) == '<';
 
+
+
+
+---store unique uris
+uniqUris = DISTINCT filteredResources;
+rmf $uniqUris
+STORE uniqUris INTO '$uniqUris' USING PigStorage();
+
+
+
+
+
+---calc namespace counts
 namespaces = FOREACH filteredResources {
 	---if url has no slashes or hashtag other than the one in http://, use complete uri as namespace..
 	namespace = (LAST_INDEX_OF($0, '/') > 8 OR LAST_INDEX_OF($0, '#') > 8 ? REGEX_EXTRACT ($0, '<(.*)[#/].*>', 1): $0);
 	GENERATE namespace;
 }
-
 groupedNamespaces = GROUP namespaces BY $0;
-
 namespaceCounts = FOREACH groupedNamespaces GENERATE group, COUNT(namespaces);
-rmf $outputFile
-STORE namespaceCounts INTO '$outputFile' USING PigStorage();
+rmf $namespaceCountsOutput
+STORE namespaceCounts INTO '$namespaceCountsOutput' USING PigStorage();
+
+
+---calc namespace counts uniq
+namespacesUniq = FOREACH uniqUris {
+    ---if url has no slashes or hashtag other than the one in http://, use complete uri as namespace..
+    namespace = (LAST_INDEX_OF($0, '/') > 8 OR LAST_INDEX_OF($0, '#') > 8 ? REGEX_EXTRACT ($0, '<(.*)[#/].*>', 1): $0);
+    GENERATE namespace;
+}
+groupedNamespacesUniq = GROUP namespacesUniq BY $0;
+namespaceUniqCounts = FOREACH groupedNamespacesUniq GENERATE group, COUNT(namespacesUniq);
+rmf $namespaceUniqCountsOutput
+STORE namespaceUniqCounts INTO '$namespaceUniqCountsOutput' USING PigStorage();
 """
 
 
