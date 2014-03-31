b7010c9955a3ff91faaf46e7d22eb6389bc4663d
Laurens Rietveld <laurens.rietveld@gmail.com>
2014-03-31 18:34:31 +0200

    added maven support first setup for running authority calculation

---
+++ src/main/java/lodanalysis/authority/CalcAuthority.java
@@ -0,0 +1,17 @@
+package lodanalysis.authority;
+
+import lodanalysis.Entry;
+
+
+
+public class CalcAuthority {
+	
+	public CalcAuthority(Entry options) {
+		System.out.println("in calc!");
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println("blaat");
+	}
+}
