189d58f266253fff5b9a9f0650cb209988d996af
Hamid R. Bazoobandi <hbazoubandi@vu.nl>
2014-05-04 17:58:42 +0200

    removed old backward chainer

--- java/backwardChainer/owl/Owl.java
+++
@@ -1,7 +0,0 @@
-package owl;
-
-public class Owl {
-  public static final String SAMEAS = "<http://www.w3.org/2002/07/owl#sameAs>";
-  public static final String EQUCLASS = "<http://www.w3.org/2002/07/owl#equivalentClass>";
-  public static final String EQUPROPERTY = "<http://www.w3.org/2002/07/owl#equivalentProperty>";
-}
--- java/backwardChainer/rdf/Rdf.java
+++
@@ -1,5 +0,0 @@
-package rdf;
-
-public class Rdf {
-  public static final String PROPERTY = "<http://www.w3.org/1999/02/22-rdf-syntax-ns#Property>";
-}
