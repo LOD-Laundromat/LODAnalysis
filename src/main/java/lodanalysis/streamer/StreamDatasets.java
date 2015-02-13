9697db5c71618f85f7b8bf9dc48047ccee4358e7
Laurens Rietveld <laurens.rietveld@gmail.com>
2015-02-13 14:37:19 +0100

    made optimialziation when running in single thread

--- src/main/java/lodanalysis/streamer/StreamDatasets.java
+++ src/main/java/lodanalysis/streamer/StreamDatasets.java
@@ -17,7 +17,7 @@ import lodanalysis.utils.Utils;
 public class StreamDatasets  extends RuneableClass {
 	public static int DELTA_ID = 2;//useful when we re-run code. We store this id in each directory. When we re-run a (possibly newer) dataset dir, we can check whether we should re-analyze this dir, or skip it
 	public static String DELTA_FILENAME = "aggregator_delta";
-	
+	private Collection<File> datasetDirs;
 	public static int TOTAL_DIR_COUNT;
 	public static int PROCESSED_COUNT = 0;
 	public static File PROVENANCE_FILE = new File(Paths.DIR_NAME_TMP + "/" + Paths.PROVENANCE);
@@ -34,35 +34,61 @@ public class StreamDatasets  extends RuneableClass {
 		if (logFile.exists()) logFile.delete();
 		LOG_FILE_WRITER = new BufferedWriter(new FileWriter(Paths.LOG_AGGREGATE));
 		
-		Collection<File> datasetDirs = entry.getDatasetDirs();
+		datasetDirs = entry.getDatasetDirs();
 		TOTAL_DIR_COUNT = datasetDirs.size();
 		int numThreads = entry.getNumThreads();
 		
-		ExecutorService executor = Executors.newFixedThreadPool(numThreads);
-		for (File datasetDir : datasetDirs) {
-			if (entry.forceExec() || Utils.getDelta(new File(entry.getMetricParentDir(), datasetDir.getName()), DELTA_FILENAME) < DELTA_ID) {
-				Runnable worker = new StreamDataset(entry, datasetDir);
-				executor.execute(worker);
-			} else {
-				if (entry.isVerbose()) System.out.println("Skipping " + datasetDir.getName() + ". Already analyzed");
-				PROCESSED_COUNT++;
-				printProgress(datasetDir);//i.e., include the unprocessed datasets (which we already processed before) in the progress counter
-				
-			}
+		if (numThreads > 1) {
+		    runMultiThreaded(numThreads);
+		} else {
+		    runSingleThread();
 		}
 		
-		// This will make the executor accept no new threads
-	    // and finish all existing threads in the queue
-	    executor.shutdown();
-	    // Wait until all threads are finish
-	    executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+		
+		
 	    
 		System.out.println();
 		LOG_FILE_WRITER.close();
 		
 	}
 	
-	public static void printProgress(File datasetDir) throws IOException {
+	private void runSingleThread() throws IOException {
+	    for (File datasetDir : datasetDirs) {
+	        if (entry.forceExec() || Utils.getDelta(new File(entry.getMetricParentDir(), datasetDir.getName()), DELTA_FILENAME) < DELTA_ID) {
+	            StreamDataset stream = new StreamDataset(entry, datasetDir);
+	            stream.run();
+	        } else {
+	            if (entry.isVerbose()) System.out.println("Skipping " + datasetDir.getName() + ". Already analyzed");
+                PROCESSED_COUNT++;
+                printProgress(datasetDir);//i.e., include the unprocessed datasets (which we already processed before) in the progress counter
+	        }
+	    }
+        
+    }
+
+    private void runMultiThreaded(int numThreads) throws IOException, InterruptedException {
+	    ExecutorService executor = Executors.newFixedThreadPool(numThreads);
+        for (File datasetDir : datasetDirs) {
+            if (entry.forceExec() || Utils.getDelta(new File(entry.getMetricParentDir(), datasetDir.getName()), DELTA_FILENAME) < DELTA_ID) {
+                Runnable worker = new StreamDataset(entry, datasetDir);
+                executor.execute(worker);
+            } else {
+                if (entry.isVerbose()) System.out.println("Skipping " + datasetDir.getName() + ". Already analyzed");
+                PROCESSED_COUNT++;
+                printProgress(datasetDir);//i.e., include the unprocessed datasets (which we already processed before) in the progress counter
+                
+            }
+        }
+        
+        // This will make the executor accept no new threads
+        // and finish all existing threads in the queue
+        executor.shutdown();
+        // Wait until all threads are finish
+        executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        
+    }
+
+    public static void printProgress(File datasetDir) throws IOException {
 		Utils.printProgress("aggregating", TOTAL_DIR_COUNT, PROCESSED_COUNT);
 	}
 	
