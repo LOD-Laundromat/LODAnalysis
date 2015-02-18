6316c72bfd558457c112049e4d6715487b00371c
Wouter Beek <w.g.j.beek@vu.nl>
2015-02-18 19:39:58 +0100

    fixed model creation script, and added verbose and non verbose version of callback

---
+++ bin/wm-callback-verbose.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+hdtQueue="/scratch/lodlaundromat/tmp/hdtQueue.txt"
+#touch /home/lodlaundromat/wm-callback.touch
+[ -z "$1" ] && echo "No dataset provided as argument" && exit 1;
+
+date >> /home/lodlaundromat/wm-callback.touch
+echo $@ >> /home/lodlaundromat/wm-callback.touch;
+
+
+
+echo "Generating HDT file ($1)"
+makeHdt $1
+
+#queue hdt file for ldf update
+echo $1 >> $hdtQueue;
+
+#analyze directory
+echo "Creating C-LOD file ($1)"
+streamDataset $1
+createModel $1
+
+exit 0;
