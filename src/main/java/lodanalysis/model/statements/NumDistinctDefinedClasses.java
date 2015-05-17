7c8f0febfe705f712c4a67b9e3e80995808ae6d8
Laurens Rietveld <laurens.rietveld@gmail.com>
2015-05-17 19:58:40 +0200

    store proper class and property count

--- src/main/java/lodanalysis/model/statements/NumDistinctDefinedClasses.java
+++ src/main/java/lodanalysis/model/statements/NumDistinctDefinedClasses.java
@@ -3,6 +3,8 @@ package lodanalysis.model.statements;
 import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
+
 import lodanalysis.Paths;
 import lodanalysis.model.CreateModel;
 import lodanalysis.model.CreateModelStatement;
@@ -20,7 +22,7 @@ public class NumDistinctDefinedClasses extends CreateModelStatement {
 
 	@Override
 	public void createDescription() throws IOException {
-		doc.addProperty(getProp(Namespace.LLM, "distinctDefinedClasses"), Integer.toString(countLines(new File(dir, Paths.DISTINCT_DEFINED_CLASSES))), XSDDatatype.XSDlong);
+		doc.addProperty(getProp(Namespace.LLM, "distinctDefinedClasses"), FileUtils.readFileToString(new File(dir, Paths.DISTINCT_DEFINED_CLASSES)), XSDDatatype.XSDlong);
 	}
 
 }
--- src/main/java/lodanalysis/model/statements/NumDistinctDefinedProperties.java
+++ src/main/java/lodanalysis/model/statements/NumDistinctDefinedProperties.java
@@ -3,6 +3,8 @@ package lodanalysis.model.statements;
 import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
+
 import lodanalysis.Paths;
 import lodanalysis.model.CreateModel;
 import lodanalysis.model.CreateModelStatement;
@@ -20,7 +22,7 @@ public class NumDistinctDefinedProperties extends CreateModelStatement {
 
 	@Override
 	public void createDescription() throws IOException {
-		doc.addProperty(getProp(Namespace.LLM, "distinctDefinedProperties"), Integer.toString(countLines(new File(dir, Paths.DISTINCT_DEFINED_PROPERTIES))), XSDDatatype.XSDlong);
+		doc.addProperty(getProp(Namespace.LLM, "distinctDefinedProperties"),FileUtils.readFileToString(new File(dir, Paths.DISTINCT_DEFINED_PROPERTIES)), XSDDatatype.XSDlong);
 	}
 
 }
