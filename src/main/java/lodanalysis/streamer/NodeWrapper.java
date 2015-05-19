26266b8f5da7dc9eae70e776c2943a8ff88fdf7e
Laurens Rietveld <laurens.rietveld@gmail.com>
2015-05-19 13:53:49 +0200

    avoid index out of bounds exception

--- src/main/java/lodanalysis/streamer/NodeWrapper.java
+++ src/main/java/lodanalysis/streamer/NodeWrapper.java
@@ -82,7 +82,7 @@ public class NodeWrapper {
 	 * This makes detection of things like lang tags and datatypes very easy and very fast.
 	 */
 	private void calcInfo() {
-		if (stringRepresentation.charAt(0) == '"') {
+		if (stringRepresentation.length() > 0 && stringRepresentation.charAt(0) == '"') {
 			this.isLiteral = true;
 			this.isUri = false;
 		}
