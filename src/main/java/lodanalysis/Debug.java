ae79efebef26c3ba186fac30381ae28554741f99
Laurens Rietveld <laurens.rietveld@gmail.com>
2015-05-20 22:21:39 +0200

    added prov-o info

--- src/main/java/lodanalysis/Debug.java
+++ src/main/java/lodanalysis/Debug.java
@@ -25,14 +25,14 @@ public class Debug {
 		 */
 //		new Entry(new String[]{"-force", "-max_size", "1", "-datasets", "datasets/", "-metrics", "metrics", "lodanalysis.streamer.StreamDatasets"});
 //		new Entry(new String[]{"-force","-dataset", "datasets/d11df3c8bc6e99c7d7a25cea746631e4", "-metrics", "metrics", "lodanalysis.streamer.StreamDatasets"});
-		new Entry(new String[]{"-force","-dataset", "datasets/testDataset", "-metrics", "metrics", "lodanalysis.streamer.StreamDatasets"});
+//		new Entry(new String[]{"-force","-dataset", "datasets/testDataset", "-metrics", "metrics", "lodanalysis.streamer.StreamDatasets"});
 
 		/**
 		 * model creator
 		 */
 		
 //		new Entry(new String[]{"-force","-metrics", "metrics", "lodanalysis.model.CreateModels"});
-//		new Entry(new String[]{"-force","-metric", "metrics/testDataset", "lodanalysis.model.CreateModels"});
+		new Entry(new String[]{"-force","-metric", "metrics/testDataset", "lodanalysis.model.CreateModels"});
 		/**
 		 * upload models
 		 */
--- src/main/java/lodanalysis/model/CreateModel.java
+++ src/main/java/lodanalysis/model/CreateModel.java
@@ -5,10 +5,40 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 import lodanalysis.Paths;
-import lodanalysis.model.statements.*;
+import lodanalysis.model.statements.Degree;
+import lodanalysis.model.statements.DegreeIn;
+import lodanalysis.model.statements.DegreeOut;
+import lodanalysis.model.statements.LengthLiteral;
+import lodanalysis.model.statements.LengthUri;
+import lodanalysis.model.statements.LengthUriObj;
+import lodanalysis.model.statements.LengthUriPred;
+import lodanalysis.model.statements.LengthUriSub;
+import lodanalysis.model.statements.NumAllLiterals;
+import lodanalysis.model.statements.NumAllUris;
+import lodanalysis.model.statements.NumBnodes;
+import lodanalysis.model.statements.NumDistinctBnodes;
+import lodanalysis.model.statements.NumDistinctClasses;
+import lodanalysis.model.statements.NumDistinctDataTypes;
+import lodanalysis.model.statements.NumDistinctDefinedClasses;
+import lodanalysis.model.statements.NumDistinctDefinedProperties;
+import lodanalysis.model.statements.NumDistinctEntities;
+import lodanalysis.model.statements.NumDistinctLanguages;
+import lodanalysis.model.statements.NumDistinctLiterals;
+import lodanalysis.model.statements.NumDistinctObjects;
+import lodanalysis.model.statements.NumDistinctProperties;
+import lodanalysis.model.statements.NumDistinctSubjects;
+import lodanalysis.model.statements.NumDistinctTriples;
+import lodanalysis.model.statements.NumDistinctUris;
+import lodanalysis.model.statements.NumObjUris;
+import lodanalysis.model.statements.NumSubUris;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
+
+import com.hp.hpl.jena.datatypes.xsd.XSDDatatype;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.Resource;
 
 public class CreateModel {
@@ -42,19 +72,72 @@ public class CreateModel {
 	public Model model;
 	public Resource voidDoc;
 	public File metricDir;
+	private Property typeProperty;
 	
-
+	private void initModel() {
+	    //init
+	    model = ModelFactory.createDefaultModel();
+	    //set namespaces
+        for (Namespace ns: Namespace.values()) {
+            model.setNsPrefix(ns.getPrefix(), ns.getUrl());
+        }
+        typeProperty = model.createProperty(Namespace.RDF.getUrl(), "type");
+        //init actual metrics resource
+        voidDoc = model.createResource(Namespace.LL.getUrl() + metricDir.getName() + "/metrics");
+        //link regular LL resource with metric resource
+        model.createResource(Namespace.LL.getUrl() + metricDir.getName()).addProperty(model.createProperty(Namespace.LLM.getUrl(), "metrics"), voidDoc);
+        
+        //set proper types
+        voidDoc.addProperty(typeProperty, model.createResource(Namespace.LLM.getUrl() + "Dataset"));
+        voidDoc.addProperty(typeProperty, model.createResource(Namespace.PROV.getUrl() + "Entity"));
+	}
+	
+	private void setProvenance() throws IOException {
+	    //get git info from sysinfo file
+	    String gitRepo = null;
+	    String commitHash = null;
+	    LineIterator it = FileUtils.lineIterator(new File(metricDir, ".sysinfo"));
+	    int count = 1;
+	    int gitRepoLine = 1;
+	    int commitHashLine = 3;
+	    while (it.hasNext()) {
+	        String line = it.next();
+	        if (count == gitRepoLine) {
+	            gitRepo = line;
+	        } else if (count == commitHashLine) {
+	            commitHash = line;
+	        }
+	        count++;
+	    }
+	    
+	    //add prov class to metric resource
+        voidDoc.addProperty(model.createProperty(Namespace.PROV.getUrl(), "used"), model.createResource(Namespace.LL.getUrl() + metricDir.getName()));
+        
+        
+        //init provenance plan (the resource referencing the github resource)
+        
+        Resource plan = model.createResource("http://lodlaundromat.org/metrics/resource/" + commitHash);
+        plan.addProperty(typeProperty, model.createResource(Namespace.PROV.getUrl() + "Plan"));
+        plan.addProperty(typeProperty, model.createResource(Namespace.PROV.getUrl() + "Entity"));
+        plan.addProperty(model.createProperty(Namespace.RDFS.getUrl() + "comment"), "SOME COMMENT", XSDDatatype.XSDstring);
+        plan.addProperty(model.createProperty(Namespace.LLM.getUrl() + "gitCommitId"), commitHash, XSDDatatype.XSDstring);
+        plan.addProperty(model.createProperty(Namespace.LLM.getUrl() + "gitRepository"), model.createResource(gitRepo));;
+        
+        //add metric calculation prov activity (referencing the prov plan)
+        Resource calculationActivity = model.createResource(Namespace.LL.getUrl() + metricDir.getName() + "/metricCalculation");
+        calculationActivity.addProperty(typeProperty, model.getResource(Namespace.PROV.getUrl() + "Activity"));
+        calculationActivity.addProperty(model.createProperty(Namespace.PROV.getUrl() + "generated"), voidDoc);
+        
+        Resource qualifiedAssociationBnode =  model.createResource();
+        qualifiedAssociationBnode.addProperty(typeProperty, model.createResource(Namespace.PROV.getUrl() + "Association"));
+        qualifiedAssociationBnode.addProperty(model.createProperty(Namespace.PROV.getUrl() + "hadPlan"), plan);
+        
+        calculationActivity.addProperty(model.createProperty(Namespace.PROV.getUrl() + "qualifiedAssociation"), qualifiedAssociationBnode);
+	}
 	public CreateModel(File metricDir) throws IOException {
 		this.metricDir = metricDir;
-		model = ModelFactory.createDefaultModel();
-		for (Namespace ns: Namespace.values()) {
-			model.setNsPrefix(ns.getPrefix(), ns.getUrl());
-		}
-
-		voidDoc = model.createResource(Namespace.LL.getUrl() + metricDir.getName() + "/metrics");
-		model.createResource(Namespace.LL.getUrl() + metricDir.getName()).addProperty(model.createProperty(Namespace.LLM.getUrl(), "metrics"), voidDoc);
-		voidDoc.addProperty(model.createProperty(Namespace.RDF.getUrl(), "type"), model.createResource(Namespace.LLM.getUrl() + "Dataset"));
-		
+		initModel();
+		setProvenance();
 		CreateModelStatement[] descriptions = new CreateModelStatement[] {
 				new NumDistinctTriples(this),
 				new NumDistinctBnodes(this),
